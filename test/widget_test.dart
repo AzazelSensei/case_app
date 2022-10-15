b2ce27f50cc634ec5fabdc9c4d67fef9d4d2f0e4
AzazelSensei <89297042+AzazelSensei@users.noreply.github.com>
2022-10-15 04:01:41 +0300

    Add reddit repository test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,32 @@
-void main() {}
+import 'package:case_app/core/network/reddit_endpoint.dart';
+import 'package:case_app/features/reddit/data/repository/reddit_repository.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group(
+    'Repository Test / ',
+    () {
+      late RedditRepository _redditRepository;
+      setUp(
+        () {
+          _redditRepository = RedditRepository(
+              Dio(BaseOptions(baseUrl: RedditEndPoint.redditPosts)));
+        },
+      );
+
+      test('GET: /get reddit request', () async {
+        try {
+          final response = await _redditRepository.getPosts();
+
+          debugPrint(response.data.toString());
+
+          expect(response.data, isNotNull);
+        } catch (e) {
+          debugPrint(e.toString());
+        }
+      });
+    },
+  );
+}
